de66fea1fd2e217403add4e10ccc09bdf46275a2
asyraf07 <asyraf.sr07@gmail.com>
2023-12-03 04:40:56 +0000

    Revert "Merge branch 'BarraBranch1' into 'main'"
    This reverts merge request !3

--- src/main/java/whizware/whizware/repository/GoodsRepository.java
+++
@@ -1,8 +0,0 @@
-package whizware.whizware.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import whizware.whizware.entity.Goods;
-@Repository
-public interface GoodsRepository extends JpaRepository<Goods, Long> {
-}
