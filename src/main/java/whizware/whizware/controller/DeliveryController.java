74d5095011086c7e4016ac590bdae428a45e911b
barra57 <muhfaishalbarra57@gmail.com>
2023-12-05 17:25:12 +0700

    Added feature Delivery (Fixed maybe)

---
+++ src/main/java/whizware/whizware/controller/DeliveryController.java
@@ -0,0 +1,66 @@
+package whizware.whizware.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import whizware.whizware.dto.BaseResponse;
+import whizware.whizware.dto.delivery.DeliveryRequest;
+import whizware.whizware.service.DeliveryService;
+
+@RestController
+@RequestMapping("/delivery")
+@RequiredArgsConstructor
+public class DeliveryController {
+
+    private final DeliveryService deliveryService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponse> getAll(@RequestParam(value = "warehouse_id", required = false) Long warehouseId) {
+        if (warehouseId == null) {
+            return ResponseEntity.ok(deliveryService.getAllDelivery());
+        }
+
+        BaseResponse response = deliveryService.getAllDeliveryByWarehouseId(warehouseId);
+        if (response.getData() == null) {
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<BaseResponse> getDeliveryById(@PathVariable Long id) {
+        BaseResponse response = deliveryService.getDeliveryById(id);
+        if (response.getData() == null) {
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> saveDelivery(@RequestBody DeliveryRequest deliveryRequest) {
+        BaseResponse response = deliveryService.saveDelivery(deliveryRequest);
+        if (response.getData() == null) {
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> updateDelivery(@PathVariable Long id, @RequestBody DeliveryRequest deliveryRequest) {
+        BaseResponse response = deliveryService.updateDelivery(id, deliveryRequest);
+        if (response.getData() == null) {
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponse> deleteDelivery(@PathVariable Long id) {
+        BaseResponse response = deliveryService.deleteDelivery(id);
+        if (response.getData() == null) {
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(response);
+    }
+}
