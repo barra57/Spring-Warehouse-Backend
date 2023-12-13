054126a0ba32f18de62fa618e45ef513534e878e
asyraf07 <asyraf.sr07@gmail.com>
2023-12-13 10:11:02 +0700

    add unit testing warehouse controller

---
+++ src/test/java/whizware/whizware/controller/WarehouseControllerTests.java
@@ -0,0 +1,248 @@
+package whizware.whizware.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import whizware.whizware.dto.BaseResponse;
+import whizware.whizware.dto.warehouse.WarehouseRequest;
+import whizware.whizware.dto.warehouse.WarehouseResponse;
+import whizware.whizware.entity.Location;
+import whizware.whizware.entity.Warehouse;
+import whizware.whizware.service.WarehouseService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+class WarehouseControllerTests {
+
+    @InjectMocks
+    WarehouseController warehouseController;
+
+    @Mock
+    WarehouseService warehouseService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    WarehouseResponse generateWarehouseResponse(Long id, String name, Long locationId) {
+        return WarehouseResponse.builder()
+                .id(id)
+                .name(name)
+                .locationId(locationId)
+                .build();
+    }
+
+    WarehouseRequest generateWarehouseRequest(String name, Long locationId) {
+        WarehouseRequest request = new WarehouseRequest();
+        request.setName(name);
+        request.setLocationId(locationId);
+        return request;
+    }
+
+    Warehouse generateWarehouse(Long id, String name, Location location) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.setId(id);
+        warehouse.setName(name);
+        warehouse.setLocation(location);
+        return warehouse;
+    }
+
+    @Test
+    void getAllWarehouses() {
+
+        List<WarehouseResponse> data = new ArrayList<>(
+                List.of(
+                        generateWarehouseResponse(1L, "asdf", 2L),
+                        generateWarehouseResponse(1L, "asdf", 2L),
+                        generateWarehouseResponse(1L, "asdf", 2L)
+                )
+        );
+
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message("berhasil")
+                .data(data)
+                .build();
+
+        when(warehouseService.getAllWarehouses()).thenReturn(expectedResponse);
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.getAll();
+
+        Assertions.assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        Assertions.assertNotNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void getWarehouseById() {
+        Long id = 1L;
+
+        String expectedMessage = "berhasil";
+        WarehouseResponse expectedData = generateWarehouseResponse(1L, "Jakarta Timur", 2L);
+
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(expectedData)
+                .build();
+
+        when(warehouseService.getWarehouseById(id)).thenReturn(expectedResponse);
+
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.getById(id);
+
+        Assertions.assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNotNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void getWarehouseByInvalidId() {
+        Long id = 1L;
+
+        String expectedMessage = "Warehouse with ID " + id + " not found";
+
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(null)
+                .build();
+
+        when(warehouseService.getWarehouseById(id)).thenReturn(expectedResponse);
+
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.getById(id);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void saveWarehouse() {
+        Long id = 1L;
+
+        WarehouseRequest request = generateWarehouseRequest("A", 2L);
+
+        String expectedMessage = "Warehouse succesfully added!";
+        WarehouseResponse expectedData = generateWarehouseResponse(id, request.getName(), request.getLocationId());
+
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(expectedData)
+                .build();
+
+        when(warehouseService.saveWarehouse(request)).thenReturn(expectedResponse);
+
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.save(request);
+
+        Assertions.assertEquals(HttpStatus.CREATED, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNotNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void saveWarehouseWithInvalidLocationId() {
+        Long id = 1L;
+        WarehouseRequest request = generateWarehouseRequest("A", 2L);
+
+        String expectedMessage = "Location with ID " + id + " not found!";
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(null)
+                .build();
+
+        when(warehouseService.saveWarehouse(request)).thenReturn(expectedResponse);
+
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.save(request);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void updateWarehouse() {
+        Long id = 1L;
+        WarehouseRequest request = generateWarehouseRequest("A", 2L);
+
+        String expectedMessage = "Warehouse succesfully updated!";
+        WarehouseResponse expectedData = generateWarehouseResponse(id, request.getName(), request.getLocationId());
+
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(expectedData)
+                .build();
+
+        when(warehouseService.updateWarehouse(id, request)).thenReturn(expectedResponse);
+
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.updated(id, request);
+
+        Assertions.assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNotNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void updateWarehouseWithInvalidId() {
+        Long id = 1L;
+        WarehouseRequest request = generateWarehouseRequest("A", 2L);
+
+        String expectedMessage = "Warehouse with id " + id + " not found!";
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(null)
+                .build();
+
+        when(warehouseService.updateWarehouse(id, request)).thenReturn(expectedResponse);
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.updated(id, request);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void deleteWarehouse() {
+        Long id = 1L;
+
+        Location location = new Location();
+        location.setId(1L);
+        location.setName("Jakarta");
+
+        String expectedMessage = "Warehouse successfully deleted!";
+        Warehouse expectedData = generateWarehouse(id, "A", location);
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(expectedData)
+                .build();
+
+        when(warehouseService.deleteWarehouse(id)).thenReturn(expectedResponse);
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.delete(id);
+
+        Assertions.assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNotNull(actualResponse.getBody().getData());
+    }
+
+    @Test
+    void deleteWarehouseWithInvalidId() {
+        Long id = 1L;
+
+        String expectedMessage = "Warehouse with id " + id + " not found!";
+        BaseResponse expectedResponse = BaseResponse.builder()
+                .message(expectedMessage)
+                .data(null)
+                .build();
+
+        when(warehouseService.deleteWarehouse(id)).thenReturn(expectedResponse);
+        ResponseEntity<BaseResponse> actualResponse = warehouseController.delete(id);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, actualResponse.getStatusCode());
+        Assertions.assertEquals(expectedMessage, actualResponse.getBody().getMessage());
+        Assertions.assertNull(actualResponse.getBody().getData());
+    }
+
+}
